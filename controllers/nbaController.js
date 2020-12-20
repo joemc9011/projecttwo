501e220ed146226245e1fde83e20e30c48c86761
joemc9011 <joemc9011@gmail.com>
2020-12-20 17:24:07 -0500

    updatejava

--- controllers/nbaController.js
+++ controllers/nbaController.js
@@ -5,70 +5,50 @@ var router = express.Router();
 // Import the model (cat.js) to use its database functions.
 var nba = require("../models/nba");
 
-router.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+router.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/indexthree.html"));
 });
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/players", function(req, res) {
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-    .then(function (response) {
-      console.log(queryURL)
-      console.log(response)
-      // for (var i = 0; i < 450; i++) {
-      //       var firstName = response[i].FirstName;
-      //       var lastName = response[i].LastName;
-      //       // var post = response.results[i].rating
-      //       // var hours = response.results[i].opening_hours.open_now
-      //       $("#response2").append("<div>", "FirstName: " + firstName, "<div>", "lastName: " + lastName, "<hr>")
-
-      //       // if ((response.results[i].price_level < 4) && (response.results[i].rating > 4)) {
-      //       //   // console.log(response.results[i].price_level)
-      //       //   // console.log(response.results[i].name)
-      //       //   $("#response2").append(name, "<div>", "Price Level: " + pricelevel, "<div>", "Rating: " + rating, "<hr>")
-      //       // }
-
-      //     }
-    })
-  nba.all(function(response) {
-    res.json({ Firstname: data });
+router.get("/players", function (req, res) {
+  nbas.all(function (data) {
+    res.json({ players: data });
   });
 });
 
-router.post("/players", function(req, res) {
-  nba.create([
-    "name", "sleepy"
+
+router.post("/players", function (req, res) {
+  nbas.create([
+    "FanDuelName", "PlayerID"
   ], [
-    req.body.name, req.body.sleepy
-  ], function(result) {
+    req.body.FanDuelName, req.body.PlayerID
+  ], function (result) {
+    res.json({ id: result.id })
     // Send back the ID of the new quote
-   });
+  });
 });
 
-router.put("/players/:id", function(req, res) {
+router.put("/players/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
-  nba.update({
-    sleepy: req.body.sleepy
-  }, condition, function(result) {
+  nbas.update({
+    player1: req.body.player1
+  }, condition, function (result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
-      res.json({ id: req.params.id});
+      res.json({ id: req.params.id });
     }
   });
 });
 
-router.delete("/players/:id", function(req, res) {
+router.delete("/players/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
-  nba.delete(condition, function(result) {
+  nbas.delete(condition, function (result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
--- public/assets/js/nba.js
+++ public/assets/js/nba.js
@@ -1,104 +1,206 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-    $.ajax("/cats", {
-      type: "GET"
-    }).then(function(data) {
-      var sleepyElem = $("#sleepyCats");
-      var nosleepyElem = $("#notSleepyCats");
-  
-      var cats = data.cats;
-      var len = cats.length;
-  
-      for (var i = 0; i < len; i++) {
-        var new_elem =
-          "<li>" +
-          cats[i].id + 
-          ". "+cats[i].name +
-          "<button class='change-sleep' data-id='" +
-          cats[i].id +
-          "' data-newsleep='" +
-          !cats[i].sleepy +
-          "'>";
-  
-        if (cats[i].sleepy) {
-          new_elem += "SLEEP TIME!";
-        } else {
-          new_elem += "WAKE UP!";
+$(document).ready(function () {
+  var queryURL = `https://api.sportsdata.io/v3/nba/scores/json/Players?key=336e712839ff463dab532b77803a63a9`
+  // console.log(queryURL);
+  // console.log(crd);
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    .then(function (response) {
+      console.log(queryURL);
+      console.log(response);
+      for (var i = 0; i < response.length; i++) {
+
+        var playerID = response[i].PlayerID;
+        var fanDuel = response[i].FanDuelName;
+        var team = response[i].Team;
+        var salary = response[i].Salary;
+        var position = response[i].Position
+        var player1Elem = $("#players");
+
+
+        if (response[i].Position == "PG" && response[i].Salary !== null) {
+                   // var post = response.results[i].rating
+          // var hours = response.results[i].opening_hours.open_now
+         
+          tr = $('<tr>');
+          tr.append("<td>" + fanDuel + "</td>");
+          tr.append("<td>" + position + "</td>");
+          tr.append("<td>" + team + "</td>");
+          tr.append("<td>" + salary + "</td>");
+          $('#pgtable').append(tr);
         }
-  
-        new_elem += "</button>";
-  
-        new_elem +=
-          "<button class='delete-cat' data-id='" +
-          cats[i].id +
-          "'>DELETE!</button></li>";
-  
-        if (cats[i].sleepy) {
-          sleepyElem.append(new_elem);
-        } else {
-          nosleepyElem.append(new_elem);
+        else if (response[i].Position == "SG" && response[i].Salary !== null) {
+          tr = $('<tr>');
+          tr.append("<td>" + fanDuel + "</td>");
+          tr.append("<td>" + position + "</td>");
+          tr.append("<td>" + team + "</td>");
+          tr.append("<td>" + salary + "</td>");
+          $('#sgtable').append(tr);
         }
+        else if (response[i].Position == "SF" && response[i].Salary !== null) {
+          tr = $('<tr>');
+          tr.append("<td>" + fanDuel + "</td>");
+          tr.append("<td>" + position + "</td>");
+          tr.append("<td>" + team + "</td>");
+          tr.append("<td>" + salary + "</td>");
+          $('#sftable').append(tr);
+        }
+        else if (response[i].Position == "PF" && response[i].Salary !== null) {
+          tr = $('<tr>');
+          tr.append("<td>" + fanDuel + "</td>");
+          tr.append("<td>" + position + "</td>");
+          tr.append("<td>" + team + "</td>");
+          tr.append("<td>" + salary + "</td>");
+          $('#pftable').append(tr);
+        }
+        else if (response[i].Position == "C" && response[i].Salary !== null) {
+          tr = $('<tr>');
+          tr.append("<td>" + fanDuel + "</td>");
+          tr.append("<td>" + position + "</td>");
+          tr.append("<td>" + team + "</td>");
+          tr.append("<td>" + salary + "</td>");
+          $('#ctable').append(tr);
+        }
+
+      
+
+        // var new_elem =
+        //   "<li>" +
+        //   playerID +
+        //   ". " + fanDuel + " ";
+        // new_elem += "</button>";
+        // new_elem +=
+        //   "<button class='deleteplayer' data-playerinfo='" +
+        //   playerID[i] +
+        //   "'>DELETE!</button></li>";
+        // player1Elem.append(new_elem);
       }
+      // console.log(new_elem);
     });
-  
-    $(document).on("click", ".change-sleep", function(event) {
-      var id = $(this).data("id");
-      var newSleep = $(this).data("newsleep")===true;
-  
-      var newSleepState = {
-        sleepy: newSleep
+
+    // pulls from mysql
+  $.ajax("/players", {
+    type: "GET"
+  }).then(function (data) {
+    var player1Elem = $("#player1");
+    var players = data.players;
+    var len = players.length;
+    for (var i = 0; i < len; i++) {
+      var new_elem =
+        "<li>" +
+        players[i].id +
+        ". " + players[i].TeamName +
+        "<button class='change-team' data-id='" +
+        players[i].id +
+        "' data-newteam='" +
+        !players[i].player1 +
+        "'>";
+      if (players[i].player1) {
+        new_elem += "BENCH TEAM ";
       };
-  
-      // Send the PUT request.
-      $.ajax("/cats/" + id, {
-        type: "PUT",
-        data: JSON.stringify(newSleepState),
-        dataType:'json',
-        contentType: 'application/json'
-      }).then(function() {
-        console.log("changed sleep to", newSleep);
-        // Reload the page to get the updated list
-        location.reload();
+      // else {
+      //   new_elem += "WAKE UP!";
+      // }
+      new_elem += "</button>";
+      new_elem +=
+        "<button class='delete-team' data-id='" +
+        players[i].id +
+        "'>DELETE!</button></li>";
+      player1Elem.append(new_elem);
+
+      $(document).on("click", ".change-team", function (event) {
+        var id = $(this).data("id");
+        var newTeam = $(this).data("newteam") === true;
+        var newTeamState = {
+          player1: newTeam,
+          player2: newTeam,
+          player3: newTeam,
+          player4: newTeam,
+          player5: newTeam
+        };
+
+
+        // Send the PUT request.
+        $.ajax("/players/" + id, {
+          type: "PUT",
+          data: JSON.stringify(newTeamState),
+          dataType: 'json',
+          contentType: 'application/json'
+        }).then(function () {
+          console.log("changed team to", newTeam);
+          // Reload the page to get the updated list
+          location.reload();
+        });
       });
-    });
-  
-    $(".create-form").on("submit", function(event) {
-      // Make sure to preventDefault on a submit event.
-      event.preventDefault();
-  
-      var newCat = {
-        name: $("#ca")
-          .val()
-          .trim(),
-        sleepy: $("[name=sleepy]:checked")
-          .val()
-          .trim()
-      };
-  
-      // Send the POST request.
-      $.ajax("/cats", {
-        type: "POST",
-        data: JSON.stringify(newCat),
-        dataType:'json',
-        contentType: 'application/json'
-      }).then(function() {
-        console.log("created new cat");
-        // Reload the page to get the updated list
-        location.reload();
+
+
+      $(".create-form").on("submit", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+        var newTeamNameEntered = {
+          PlayerID: playerID,
+          FanDuelName: response[i].FanDuelName
+          // TeamName: $("#ca").val().trim(),
+          // sleepy: $("[name=sleepy]:checked")
+          //   .val()
+          //   .trim()
+        };
+        // Send the POST request.
+        $.ajax("/players", {
+          type: "POST",
+          data: JSON.stringify(newTeamNameEntered),
+          dataType: 'json',
+          contentType: 'application/json'
+        }).then(function () {
+          console.log("created new team");
+          // Reload the page to get the updated list
+          location.reload();
+        });
       });
-    });
-  
-    $(document).on("click", ".delete-cat", function(event) {
-      var id = $(this).data("id");
-  
-      // Send the DELETE request.
-      $.ajax("/cats/" + id, {
-        type: "DELETE"
-      }).then(function() {
-        console.log("deleted cat", id);
-        // Reload the page to get the updated list
-        location.reload();
+
+
+      $(document).on("click", ".deleteplayer", function (event) {
+        var id = $(this).data("playerinfo");
+        // Send the DELETE request.
+        $.ajax("/players/" + id, {
+          type: "DELETE",
+          dataType: 'json'
+        }).then(function () {
+          console.log("deleted team", id);
+          // Reload the page to get the updated list
+          location.reload();
+        });
       });
-    });
+    }
+  })
+});
+
+
+$(function() {
+
+  /* Get all rows from your 'table' but not the first one 
+   * that includes headers. */
+  var rows = $('tr').not(':first');
+
+  /* Create 'click' event handler for rows */
+  rows.on('click', function(e) {
+
+      /* Get current row */
+      var row = $(this);
+
+      /* Check if 'Ctrl', 'cmd' or 'Shift' keyboard key was pressed
+       * 'Ctrl' => is represented by 'e.ctrlKey' or 'e.metaKey'
+       * 'Shift' => is represented by 'e.shiftKey' */
+      if ((e.ctrlKey || e.metaKey) || e.shiftKey) {
+          /* If pressed highlight the other row that was clicked */
+          row.addClass('highlight');
+      } else {
+          /* Otherwise just highlight one row and clean others */
+          rows.removeClass('highlight');
+          row.addClass('highlight');
+      }
   });
-  
+
+});
