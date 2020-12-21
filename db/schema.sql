37ba03ed1e0c9c8aa5250491296a314b5f5c1bae
Brian Cardines <bcardines811@gmail.com>
2020-12-20 19:55:20 -0500

    workingProduct!

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,11 @@
+DROP DATABASE IF EXISTS nba;
 CREATE DATABASE nba;
 USE nba;
 
 CREATE TABLE players (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255),
-    position VARCHAR(255),
-    team VARCHAR(255),
+    id INT NOT NULL AUTO_INCREMENT,    
+    lastName VARCHAR(255),    
+    firstName VARCHAR(255),
+	starter BOOLEAN DEFAULT true,    
     PRIMARY KEY (id)
 );
-
--- public/assets/js/nbajs.js
+++ public/assets/js/nbajs.js
@@ -1,101 +1,101 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function () {
-  // var queryURL = `https://api.sportsdata.io/v3/nba/scores/json/Players?key=336e712839ff463dab532b77803a63a9`
-  // // console.log(queryURL);
-  // // console.log(crd);
-  // $.ajax({
-  //   url: queryURL,
-  //   method: "GET"
-  // })
-  //   .then(function (response) {
-  //     console.log(queryURL)
-  //     console.log(response)
-  //     for (var i = 0; i < response.length; i++) {
-  //       // var fanDuelName = response[i].FanDuelName;
-  //       // var playerID = response[i].PlayerID;
-  //       var firstName = response[i].FirstName;
-  //       var lastName2 = response[i].LastName;
-  //       var team = response[i].Team;
-  //       var salary = response[i].Salary;
-  //       var position = response[i].Position
-  //       // var player1Elem = $("#players");
+  $.ajax("/players", {
+    type: "GET"
+  }).then(function (data) {
+    var startersEl = $("#starters");
+    var benchEl = $("#bench");
+    // var notEatenElem = $("#notEatenBurger");
 
-  //       if (response[i].Position == "PG" && response[i].Salary !== null) {
-  //         $("#pgfirstname").append("<td>", firstName, "</td>");
-  //         $("#pglastname").append("<td>", lastName2, "</td>");
-  //         $("#pgposition").append("<td>", position, "</td>");
-  //         $("#pgteam").append("<td>", team, "</td>");
-  //         $("#pgsalary").append("<td>", salary, "</td>");
-  //       }
-  //       else if (response[i].Position == "SG" && response[i].Salary !== null) {
-  //         $("#sgfirstname").append("<td>", firstName, "</td>");
-  //         $("#sglastname").append("<td>", lastName2, "</td>");
-  //         $("#sgposition").append("<td>", position, "</td>");
-  //         $("#sgteam").append("<td>", team, "</td>");
-  //         $("#sgsalary").append("<td>", salary, "</td>");
-  //       }
-  //       else if (response[i].Position == "SF" && response[i].Salary !== null) {
-  //         $("#sffirstname").append("<td>", firstName, "</td>");
-  //         $("#sflastname").append("<td>", lastName2, "</td>");
-  //         $("#sfposition").append("<td>", position, "</td>");
-  //         $("#sfteam").append("<td>", team, "</td>");
-  //         $("#sfsalary").append("<td>", salary, "</td>");
-  //       }
-  //       else if (response[i].Position == "PF" && response[i].Salary !== null) {
-  //         $("#pffirstname").append("<td>", firstName, "</td>");
-  //         $("#pflastname").append("<td>", lastName2, "</td>");
-  //         $("#pfposition").append("<td>", position, "</td>");
-  //         $("#pfteam").append("<td>", team, "</td>");
-  //         $("#pfsalary").append("<td>", salary, "</td>");
-  //       }
-  //       else if (response[i].Position == "C" && response[i].Salary !== null) {
-  //         $("#cfirstname").append("<td>", firstName, "</td>");
-  //         $("#clastname").append("<td>", lastName2, "</td>");
-  //         $("#cposition").append("<td>", position, "</td>");
-  //         $("#cteam").append("<td>", team, "</td>");
-  //         $("#csalary").append("<td>", salary, "</td>");
-  //       };
+    var players = data.players;
+    var len = players.length;
 
-        $.ajax("/players", {
-          type: "GET"
-        }).then(function (data) {
-          var startersEl = $("#starters");
-          var benchEl = $("#bench");
-          // var notEatenElem = $("#notEatenBurger");
+    for (var i = 0; i < players.length; i++) {
+      var new_elem =
+        "<li>" +
+        players[i].id +
+        ". " + players[i].lastName +
+        "<button class='change-roster' data-id='" +
+        players[i].id +
+        "' data-reserve='" +
+        !players[i].starter +
+        "'>";
 
-          var players = data.players;
-          var len = players.length;
+      if (players[i].starter) {
+        new_elem += "starter";
+      } else {
+        new_elem += "bench";
+      }
 
-          for (var i = 0; i < players.length; i++) {
-            var new_elem =
-              "<li>" +
-              players[i].id +
-              ". " + players[i].lastName +
-              "<button class='change-roster' data-id='" +
-              players[i].id +
-              "' data-reserve='" +
-              !players[i].starter +
-              "'>";
+      new_elem += "</button>";
 
-            if (players[i].starter) {
-              new_elem += "starter";
-            } else {
-              new_elem += "bench";
-            }
+      new_elem +=
+        "<button class='delete-player' data-id='" +
+        players[i].id +
+        "'>Delete!!</button></li>";
 
-            new_elem += "</button>";
+      if (players[i].starter) {
+        startersEl.append(new_elem);
+      } else {
+        benchEl.append(new_elem);
+      }
+    }
 
-            new_elem +=
-              "<button class='delete-player' data-id='" +
-              players[i].id +
-              "'>Delete!!</button></li>";
+    var queryURL = `https://api.sportsdata.io/v3/nba/scores/json/Players?key=336e712839ff463dab532b77803a63a9`
+    // console.log(queryURL);
+    // console.log(crd);
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      .then(function (response) {
+        console.log(queryURL)
+        console.log(response)
+        for (var i = 0; i < response.length; i++) {
+          // var fanDuelName = response[i].FanDuelName;
+          // var playerID = response[i].PlayerID;
+          var firstName = response[i].FirstName;
+          var lastName2 = response[i].LastName;
+          var team = response[i].Team;
+          var salary = response[i].Salary;
+          var position = response[i].Position
+          // var player1Elem = $("#players");
 
-            if (players[i].starter) {
-              startersEl.append(new_elem);
-            } else {
-              benchEl.append(new_elem);
-            }
+          if (response[i].Position == "PG" && response[i].Salary !== null) {
+            $("#pgfirstname").append("<td>", firstName, "</td>");
+            $("#pglastname").append("<td>", lastName2, "</td>");
+            $("#pgposition").append("<td>", position, "</td>");
+            $("#pgteam").append("<td>", team, "</td>");
+            $("#pgsalary").append("<td>", salary, "</td>");
           }
+          else if (response[i].Position == "SG" && response[i].Salary !== null) {
+            $("#sgfirstname").append("<td>", firstName, "</td>");
+            $("#sglastname").append("<td>", lastName2, "</td>");
+            $("#sgposition").append("<td>", position, "</td>");
+            $("#sgteam").append("<td>", team, "</td>");
+            $("#sgsalary").append("<td>", salary, "</td>");
+          }
+          else if (response[i].Position == "SF" && response[i].Salary !== null) {
+            $("#sffirstname").append("<td>", firstName, "</td>");
+            $("#sflastname").append("<td>", lastName2, "</td>");
+            $("#sfposition").append("<td>", position, "</td>");
+            $("#sfteam").append("<td>", team, "</td>");
+            $("#sfsalary").append("<td>", salary, "</td>");
+          }
+          else if (response[i].Position == "PF" && response[i].Salary !== null) {
+            $("#pffirstname").append("<td>", firstName, "</td>");
+            $("#pflastname").append("<td>", lastName2, "</td>");
+            $("#pfposition").append("<td>", position, "</td>");
+            $("#pfteam").append("<td>", team, "</td>");
+            $("#pfsalary").append("<td>", salary, "</td>");
+          }
+          else if (response[i].Position == "C" && response[i].Salary !== null) {
+            $("#cfirstname").append("<td>", firstName, "</td>");
+            $("#clastname").append("<td>", lastName2, "</td>");
+            $("#cposition").append("<td>", position, "</td>");
+            $("#cteam").append("<td>", team, "</td>");
+            $("#csalary").append("<td>", salary, "</td>");
+          };
 
 
           $(document).on("click", ".change-roster", function (event) {
@@ -152,5 +152,5 @@ $(function () {
               // Reload the page to get the updated list
               location.reload();
             });
-          })})
-        })
+          })}})}
+          )})
