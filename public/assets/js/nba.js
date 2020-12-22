10cf9e1272be0a6914ac1aa060dcc25334dca93c
joemc9011 <joemc9011@gmail.com>
2020-12-21 20:25:59 -0500

    final

--- public/assets/js/nba.js
+++ public/assets/js/nba.js
@@ -178,29 +178,4 @@ $(document).ready(function () {
 });
 
 
-$(function() {
 
-  /* Get all rows from your 'table' but not the first one 
-   * that includes headers. */
-  var rows = $('tr').not(':first');
-
-  /* Create 'click' event handler for rows */
-  rows.on('click', function(e) {
-
-      /* Get current row */
-      var row = $(this);
-
-      /* Check if 'Ctrl', 'cmd' or 'Shift' keyboard key was pressed
-       * 'Ctrl' => is represented by 'e.ctrlKey' or 'e.metaKey'
-       * 'Shift' => is represented by 'e.shiftKey' */
-      if ((e.ctrlKey || e.metaKey) || e.shiftKey) {
-          /* If pressed highlight the other row that was clicked */
-          row.addClass('highlight');
-      } else {
-          /* Otherwise just highlight one row and clean others */
-          rows.removeClass('highlight');
-          row.addClass('highlight');
-      }
-  });
-
-});
